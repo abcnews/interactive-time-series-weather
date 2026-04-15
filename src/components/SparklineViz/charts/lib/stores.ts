b224b4390145ad3954ce6739951466f2fa21894c
Ash Kyd <kyd.ashley@abc.net.au>
2026-04-15 11:07:09 +1000

    bug: dead code

--- src/components/SparklineViz/charts/lib/stores.ts
+++ src/components/SparklineViz/charts/lib/stores.ts
@@ -38,38 +38,6 @@ export const extentStores = Object.fromEntries(
   })
 );
 
-export const devmode = readable(false, (set, update) => {
-  let devmodeToggleCount = 0;
-  let devmodeCountTimeout: ReturnType<typeof setTimeout> | undefined;
-
-  const listener = (evt: KeyboardEvent) => {
-    if (evt.key === 'D') {
-      devmodeToggleCount++;
-
-      if (!devmodeCountTimeout) {
-        devmodeCountTimeout = setTimeout(() => {
-          devmodeToggleCount = 0;
-          devmodeCountTimeout = undefined;
-        }, 1000);
-      }
-      if (devmodeToggleCount > 2) {
-        update(current => {
-          return !current;
-        });
-        clearTimeout(devmodeCountTimeout);
-        devmodeToggleCount = 0;
-      }
-    }
-  };
-
-  addEventListener('keyup', listener);
-
-  return () => {
-    removeEventListener('keyup', listener);
-    clearTimeout(devmodeCountTimeout);
-  };
-});
-
 export const activeObservation = writable<null | ObservationType>(null);
 
 /** When this is true we don't close the popup in the WeatherChart root because the click came from an interaction with the chart. */
