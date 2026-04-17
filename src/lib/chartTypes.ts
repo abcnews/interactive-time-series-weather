3d8ee24d21aa4f13ac8368cf241a74a24b1888c0
Ash Kyd <kyd.ashley@abc.net.au>
2026-04-17 14:48:28 +1000

    bug: why are the types here lol

--- src/lib/chartTypes.ts
+++ src/lib/chartTypes.ts
@@ -1,71 +1,7 @@
-import {
-  type InferOutput,
-  array,
-  date,
-  union,
-  isoDateTime,
-  nullable,
-  number,
-  object,
-  pipe,
-  string,
-  transform,
-  literal
-} from 'valibot';
 import { BASE_URL } from './util';
-
-// --- Constants ---
+import type { MetricType, MetricProps } from '../types';
 
 const METRAWEATHER_ATTR = "Times shown in user's local time. Source: MetraWeather.";
-
-// --- Schemas & Types ---
-
-export const ObservationSchema = object({
-  local_date_time: string(),
-  aifstime_utc: pipe(
-    string(),
-    transform(str => {
-      const segments = str.match(/.{2}/g);
-      if (!segments) {
-        throw new Error('Invalid date string');
-      }
-      return `${segments[0]}${segments[1]}-${segments[2]}-${segments[3]}T${segments[4]}:${segments[5]}:${segments[6]}Z`;
-    }),
-    transform(str => new Date(Date.parse(str))),
-    date()
-  ),
-  gust_kmh: nullable(number()),
-  air_temp: number(),
-  wind_dir: string(),
-  wind_spd_kmh: number(),
-  rain_trace_accumulation: nullable(number())
-});
-
-export const LocationSchema = object({
-  name: string(),
-  refresh_message: string(),
-  observations: array(ObservationSchema)
-});
-
-export type ObservationType = InferOutput<typeof ObservationSchema>;
-
-export const MetricSchema = union([literal('gust'), literal('rainSince9am'), literal('humidity'), literal('tempc')]);
-export type MetricType = InferOutput<typeof MetricSchema>;
-
-export type MetricProps = {
-  name: string;
-  editorialNotes: string;
-  parseValue?: (value: number) => number;
-  formatValue: (value: number) => string;
-  dataUrl: string;
-  attribution: string;
-  colour: string;
-  darkColour: string;
-  yMin?: number;
-  yMax?: number;
-};
-
-// --- Metric Configuration (The "One-Stop Shop") ---
 
 export const metricProperties: Record<MetricType, MetricProps> = {
   gust: {
